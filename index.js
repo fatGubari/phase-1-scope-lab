3eea2b9897a4837fdd50ed40bf3bdac588adf124
fatima abdullah <fatimajubary@gmail.com>
2023-01-07 21:49:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+bestCustomer
+setBestCustomer()
+function setBestCustomer(){
+   bestCustomer= 'not bob'
+}
+
+overwriteBestCustomer()
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 55
+        leastFavoriteCustomer = 99
+}
